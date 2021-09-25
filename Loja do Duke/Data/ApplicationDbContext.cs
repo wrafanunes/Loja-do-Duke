a39733aa7806d338ee303c6940daf6cedc8f6e71
wrafanunes <wrsnunes@gmail.com>
2021-09-24 22:57:29 -0300

    criando as configurações básicas do Identity e adicionando a migração do EntityFrameWorkCore

--- Loja do Duke/Data/ApplicationDbContext.cs
+++ Loja do Duke/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Loja_do_Duke.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Loja_do_Duke.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- Loja do Duke/Startup.cs
+++ Loja do Duke/Startup.cs
@@ -32,7 +32,7 @@ namespace Loja_do_Duke
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")
                     ));
-            
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +52,7 @@ namespace Loja_do_Duke
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

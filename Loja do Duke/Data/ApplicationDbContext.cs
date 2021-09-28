d71d411844d62ef5c900ca51193d8a147742ad57
wrafanunes <wrsnunes@gmail.com>
2021-09-28 20:06:45 -0300

    adicionando a propriedade Name na tabela de usuários

--- Loja do Duke/Data/ApplicationDbContext.cs
+++ Loja do Duke/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace Loja_do_Duke.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Supply> Supplies { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ Loja do Duke/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}

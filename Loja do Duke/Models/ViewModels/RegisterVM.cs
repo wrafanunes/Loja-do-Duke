84c6b544334ca7fbd7e12ecca7d1c0655c87aa31
wrafanunes <wrsnunes@gmail.com>
2021-09-28 20:19:33 -0300

    criando as viewmodels de login e registro e o controlador de Conta

---
+++ Loja do Duke/Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Models.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {1} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "As senhas digitadas divergem.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}

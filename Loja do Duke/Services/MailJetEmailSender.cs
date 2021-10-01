be5ccc30d5da0724ba07e423357b77c7863bdaaa
wrafanunes <wrsnunes@gmail.com>
2021-09-30 21:50:39 -0300

    implementando a API Mailjet para envio de e-mails

---
+++ Loja do Duke/Services/MailJetEmailSender.cs
@@ -0,0 +1,80 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Services
+{
+    public class MailJetEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public MailJetOptions _options;
+
+        public MailJetEmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            _options = _configuration.GetSection("MailJet").Get<MailJetOptions>();
+            MailjetClient client = new MailjetClient(_options.ApiKey, _options.SecretKey)
+            {
+
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "wrsnunes@gmail.com"},
+        {"Name", "Wilson"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          "wrsnunes@gmail.com"
+         }, {
+          "Name",
+          "Wilson"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      },  {
+       "HTMLPart",
+       htmlMessage
+      },
+     }
+             });
+            MailjetResponse response = await client.PostAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
+                Console.WriteLine(response.GetData());
+            }
+            else
+            {
+                Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+                Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+                Console.WriteLine(response.GetData());
+                Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+            }
+        }
+    }
+}
+
---
+++ Loja do Duke/Services/MailJetOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Services
+{
+    public class MailJetOptions
+    {
+        public string ApiKey { get; set; }
+        public string SecretKey { get; set; }
+    }
+}

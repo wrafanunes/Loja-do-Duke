ae9877110e52f43a09b94bdf5d3e333fb0bac09e
wrafanunes <wrsnunes@gmail.com>
2021-11-18 23:21:12 -0300

    implementando o Controller de Roles e as Views de lista de usuários e de Roles

---
+++ Loja do Duke/Controllers/RolesController.cs
@@ -0,0 +1,30 @@
+﻿using Loja_do_Duke.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _user;
+        private readonly RoleManager<IdentityRole> _role;
+
+        public RolesController(ApplicationDbContext context, UserManager<IdentityUser> user, RoleManager<IdentityRole> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _context.Roles.ToList();
+            return View(roles);
+        }
+    }
+}

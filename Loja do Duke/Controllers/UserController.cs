08d3174b17328c345329f207a817d93c4f878e02
wrafanunes <wrsnunes@gmail.com>
2021-11-18 21:59:09 -0300

    implementando o Controller de Usuário com o método Index

---
+++ Loja do Duke/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Loja_do_Duke.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _user;
+
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> user)
+        {
+            _context = context;
+            _user = user;
+        }
+
+        public IActionResult Index()
+        {
+            var userList = _context.ApplicationUsers.ToList();
+            var userRoles = _context.UserRoles.ToList();
+            var roles = _context.Roles.ToList();
+            foreach (var user in userList)
+            {
+                var role = userRoles.FirstOrDefault(u => u.UserId == user.Id);
+                if (role == null)
+                {
+                    user.Role = "None";
+                }
+                else
+                {
+                    user.Role = roles.FirstOrDefault(u => u.Id == role.RoleId).Name;
+                }
+            }
+            return View(userList);
+        }
+    }
+}

31a97e144a51d04740ddb17e9b2673281fadcccb
wrafanunes <wrsnunes@gmail.com>
2021-11-20 19:10:10 -0300

    inserindo Store e ViewModel de Claim

---
+++ Loja do Duke/Data/ClaimStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Data
+{
+    public static class ClaimStore
+    {
+        public static List<Claim> claims = new List<Claim>()
+        {
+            new Claim("Create","Create"),
+            new Claim("Edit","Edit"),
+            new Claim("Delete","Delete")
+        };
+    }
+}
---
+++ Loja do Duke/Models/ViewModels/UserClaimsVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Models.ViewModels
+{
+    public class UserClaimsVM
+    {
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+        public UserClaimsVM()
+        {
+            Claims = new List<UserClaim>();
+        }
+    }
+    
+    public class UserClaim
+    {
+        public string Type { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}

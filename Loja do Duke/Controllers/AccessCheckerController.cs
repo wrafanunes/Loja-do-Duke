e4ea340c81a3816322e566761b2627eb0b831958
wrafanunes <wrsnunes@gmail.com>
2021-11-21 18:59:14 -0300

    criando o Controller e as Views de verificador de acesso

---
+++ Loja do Duke/Controllers/AccessCheckerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_do_Duke.Controllers
+{
+    public class AccessCheckerController : Controller
+    {
+
+        public IActionResult AllAccess()
+        {
+            return View();
+        }
+
+        public IActionResult AuthorizedAccess()
+        {
+            return View();
+        }
+
+        public IActionResult UserAccess()
+        {
+            return View();
+        }
+
+        public IActionResult AdminAccess()
+        {
+            return View();
+        }
+
+        public IActionResult Admin_CreateAccess()
+        {
+            return View();
+        }
+
+        public IActionResult Admin_Create_Edit_DeleteAccess()
+        {
+            return View();
+        }
+
+        public IActionResult Admin_Create_Edit_DeleteAccess_SuperAdmin()
+        {
+            return View();
+        }
+    }
+}
